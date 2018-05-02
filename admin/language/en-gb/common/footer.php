b3f52501f8b1df60fd79980400a5db5c1677930d
root <root@mbdvm.zxtteulk52zunaw0o3m4mfjmld.ix.internal.cloudapp.net>
2018-05-02 06:50:48 +0000

    Adding Files

---
+++ admin/language/en-gb/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a href="http://20.184.24.128/">MBD Store</a> &copy; All Rights Reserved.';
+$_['text_version'] = 'Version %s';
---
+++ admin/language/id-id/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a href="http://20.184.24.128/">MBD Store</a> &copy; Hak cipta dilindungi Undang-undang';
+$_['text_version'] = 'Version %s';
